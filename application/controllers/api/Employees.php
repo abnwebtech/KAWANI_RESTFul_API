68c5180185035088badf029709f0508705f10096
Cristhian Kevin Sagun <cristhiansagun@gmail.com>
2017-06-02 15:08:28 +0800

    KAWANI - REST API
    Barebone stup of kawani restful api server.

---
+++ application/controllers/api/Employees.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+/**
+ * Some class description here...
+ *
+ * @package     KAWANI
+ * @subpackage  subpackage
+ * @category    category
+ * @author      cristhian.kevin@systemantech.com
+ * @link        http://systemantech.com
+ */
+class Employees extends REST_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('employee_model');
+    }
+
+    /**
+     * Some description here
+     *
+     * @param       param
+     * @return      return
+     */
+    public function index_get()
+    {
+        $employee_id = $this->uri->segment(3);
+        $employee = $this->employee_model->get_by(['id' => $employee_id]);
+
+        if (isset($employee['id'])) {
+            $this->response([
+                'status'  => 'success',
+                'message' => 'Successfully get data!',
+                'data'    => $employee
+            ]);
+        } else {
+            $this->response([
+                'status'  => 'success',
+                'message' => 'Successfully get data!',
+                'data'    => $employee
+            ], REST_Controller::HTTP_OK);
+        }
+    }
+}
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Some class description here...
+ *
+ * @package     KAWANI
+ * @subpackage  subpackage
+ * @category    category
+ * @author      cristhian.kevin@systemantech.com
+ * @link        http://systemantech.com
+ */
+class MY_Controller extends CI_Controller {
+
+    /**
+     * Some description here
+     *
+     * @param       param
+     * @return      return
+     */
+    function __construct()
+    {
+        parent::__construct();
+    }
+}
